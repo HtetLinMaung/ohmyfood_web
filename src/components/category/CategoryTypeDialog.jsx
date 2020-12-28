254c73905aec9120edb2e1baddc95f755c6d57c3
htetlinmaung <htetlinmaung@htetlinmaungs-MacBook-Pro.local>
2020-12-29 06:07:24 +0630

    update

--- src/components/category/CategoryTypeDialog.jsx
+++ src/components/category/CategoryTypeDialog.jsx
@@ -2,24 +2,73 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import Dialog from "@material-ui/core/Dialog";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogActions from "@material-ui/core/DialogActions";
 import TextField from "../form/TextField";
 import { Grid } from "@material-ui/core";
+import Button from "@material-ui/core/Button";
+import ImageUploader from "../upload/ImageUploader";
 
 const useStyles = makeStyles(() => ({
-  root: {},
+  button: {
+    textTransform: "capitalize",
+    borderRadius: 10,
+    fontSize: 12,
+    width: "40%",
+  },
 }));
 
 const CategoryTypeDialog = ({ open, onClose }) => {
   const classes = useStyles();
 
+  const uploadHandler = (file) => {};
+
+  const saveHandler = () => {
+    onClose();
+  };
+
   return (
     <Dialog open={open} onClose={onClose}>
-      <DialogTitle>New Category Type</DialogTitle>
-      <Grid container spacing={1}>
-        <Grid item>
-          <TextField raised={true} />
+      <DialogTitle style={{ textAlign: "center" }}>
+        New Category Type
+      </DialogTitle>
+      <DialogContent style={{ padding: "0 3rem" }}>
+        <Grid
+          container
+          spacing={1}
+          justify="center"
+          style={{ marginBottom: "1rem" }}
+        >
+          <Grid item>
+            <ImageUploader width={115} height={120} onUpload={uploadHandler} />
+          </Grid>
         </Grid>
-      </Grid>
+
+        <Grid
+          container
+          spacing={1}
+          justify="center"
+          style={{ marginBottom: "2rem" }}
+        >
+          <Grid item>
+            <TextField placeholder="Name" raised={true} />
+          </Grid>
+        </Grid>
+      </DialogContent>
+
+      <DialogActions style={{ justifyContent: "center" }}>
+        <Button variant="outlined" className={classes.button} onClick={onClose}>
+          Cancel
+        </Button>
+        <Button
+          variant="contained"
+          className={classes.button}
+          style={{ background: "#3696F8", color: "#fff" }}
+          onClick={saveHandler}
+        >
+          Save
+        </Button>
+      </DialogActions>
     </Dialog>
   );
 };
---
+++ src/components/upload/ImageUploader.jsx
@@ -0,0 +1,70 @@
+import React, { useRef, useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+
+const useStyles = makeStyles(() => ({
+  root: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    cursor: "pointer",
+    borderRadius: 10,
+    background: "#fff",
+    textTransform: "capitalize",
+    backgroundSize: "cover",
+    fontSize: 12,
+    "&:hover": {
+      backgroundSize: "cover",
+      background: "#fff",
+    },
+    "&:active": {
+      backgroundSize: "cover",
+      background: "#fff",
+    },
+  },
+}));
+
+const ImageUploader = ({ width, height, onUpload, src }) => {
+  const [imageUrl, setImageUrl] = useState(src);
+  const inputEl = useRef(null);
+  const classes = useStyles();
+
+  const handleClick = () => {
+    inputEl.current.click();
+  };
+
+  const handleFileChanged = (e) => {
+    const { files } = e.target;
+    if (files[0]) {
+      onUpload(files[0]);
+      const fileReader = new FileReader();
+      fileReader.addEventListener("load", (e) => {
+        setImageUrl(e.target.result);
+      });
+      fileReader.readAsDataURL(files[0]);
+    }
+  };
+
+  return (
+    <Button
+      variant="outlined"
+      className={classes.root}
+      style={{
+        width: width,
+        height: height,
+        backgroundImage: `url('${imageUrl}')`,
+      }}
+      onClick={handleClick}
+    >
+      Image
+      <input
+        ref={inputEl}
+        type="file"
+        style={{ display: "none" }}
+        onChange={handleFileChanged}
+      />
+    </Button>
+  );
+};
+
+export default ImageUploader;
